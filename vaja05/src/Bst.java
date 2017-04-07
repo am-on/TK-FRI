d702183e93cb8d19d11bc0f75b5aca5d771ba762
am-on <igre@outlook.com>
2017-04-07 23:01:56 +0200

    do izziv 5

---
+++ vaja05/src/Bst.java
@@ -0,0 +1,183 @@
+
+public class Bst<Tip extends Comparable> implements Seznam<Tip> {
+
+    class ElementBST {
+
+        public Tip value;
+        public ElementBST left, right;
+
+        public ElementBST(Tip e) {
+            this(e, null, null);
+        }
+
+        public ElementBST(Tip e, ElementBST lt, ElementBST rt) {
+            value = e;
+            left = lt;
+            right = rt;
+        }
+    }
+
+    ElementBST rootNode;
+    private Tip minNodeValue;
+
+    public Bst() {
+        rootNode = null;
+    }
+
+    private boolean member(Tip e) {
+        return member(e, rootNode);
+    }
+
+    private boolean member(Tip e, ElementBST node) {
+        if (node == null) {
+            return false;
+        } else if (e.compareTo(node.value) == 0) {
+            return true;
+        } else if (e.compareTo(node.value) < 0) {
+            return member(e, node.left);
+        } else {
+            return member(e, node.right);
+        }
+    }
+
+    private void insert(Tip e) {
+        rootNode = insertLeaf(e, rootNode);
+    }
+
+    private void delete(Tip e) {
+        rootNode = delete(e, rootNode);
+    }
+
+    private ElementBST insertLeaf(Tip e, ElementBST node) {
+        if (node == null) {
+            node = new ElementBST(e);
+        } else if (e.compareTo(node.value) < 0) {
+            node.left = insertLeaf(e, node.left);
+        } else if (e.compareTo(node.value) > 0) {
+            node.right = insertLeaf(e, node.right);
+        } else {
+            throw new java.lang.IllegalArgumentException(); //element ze obstaja
+        }
+        return node;
+    }
+
+    // TO DO
+    private ElementBST delete(Tip e, ElementBST node) {
+
+        if (node.value.compareTo(e) == 0) {
+            if (node.left == null && node.right == null) {
+                return null;
+            } else if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            }
+            else {
+
+                ElementBST minNode = node.right;
+                if (minNode.left == null) {
+                    minNode.left = node.left;
+                    return minNode;
+                }
+
+                ElementBST befMin = minNode;
+                while(minNode.left != null) {
+                    befMin = minNode;
+                    minNode = minNode.left;
+                }
+
+                befMin.left = minNode.right;
+                minNode.left = node.left;
+                minNode.right = node.right;
+
+                return minNode;
+            }
+        }
+
+        if (node.value.compareTo(e) > 0)
+            node.left = delete(e, node.left);
+        else
+            node.right = delete(e, node.right);
+
+        return node;
+
+    }
+
+    private ElementBST deleteMin(ElementBST node) {
+        ElementBST minNode = node;
+        while (minNode.left != null) {
+            minNode = minNode.left;
+        }
+
+        return delete(minNode.value, minNode);
+    }
+
+    private int getDepth(ElementBST node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(getDepth(node.left), getDepth(node.right));
+    }
+
+    private int countNodes(ElementBST node) {
+        if (node == null) {
+            return 0;
+        }
+        int i = 1 + countNodes(node.left) + countNodes(node.right);
+        return i;
+    }
+
+    @Override
+    public void add(Tip e) {
+        insert(e);
+    }
+
+    @Override
+    public Tip removeFirst() {
+        if (this.isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+        Tip el = rootNode.value;
+        delete(rootNode.value);
+        return el;
+    }
+
+    @Override
+    public Tip getFirst() {
+        if (this.isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+        return rootNode.value;
+    }
+
+    @Override
+    public int size() {
+        int i = countNodes(rootNode);
+        return i;
+    }
+
+    @Override
+    public int depth() {
+        return getDepth(rootNode);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (rootNode == null);
+    }
+
+    @Override
+    public Tip remove(Tip e) {
+        if (!this.exists(e)) {
+            throw new java.util.NoSuchElementException();
+        } else {
+            delete(e);
+        }
+        return e;
+    }
+
+    @Override
+    public boolean exists(Tip e) {
+        return member(e);
+    }
+}
---
+++ vaja05/test/SeznamiUVTest.java
@@ -0,0 +1,327 @@
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SeznamiUVTest {
+
+    private SeznamiUV uv;
+
+    public SeznamiUVTest() {
+    }
+
+    @Before
+    public void setUp() {
+        uv = new SeznamiUV();
+    }
+
+    // TO DO 
+    // razsirite integracijske teste za metodo Use
+    @Test
+    public void testUse() {
+        assertEquals("OK", uv.processInput("use pv"));
+        assertEquals("OK", uv.processInput("use sk"));
+        assertEquals("OK", uv.processInput("use bst"));
+    }
+
+    @Test
+    public void testUseEmpty() {
+        assertEquals("Error: please specify a data structure type {pv|sk|bst}", uv.processInput("use"));
+    }
+
+    @Test
+    public void testUseUnexisting() {
+        assertEquals("Error: please specify a correct data structure type {pv|sk|bst}", uv.processInput("use test"));
+    }
+    // TO DO
+    // napišite teste za sklad, prioritetno vrsto in BS drevo
+    // testi kličejo (zaporedoma) vse operacije nad določeno strukturo 
+    // glej POMOZNE METODE
+
+
+    private void setUpBst() {
+        setUp();
+
+    }
+
+    @Test
+    public void testUseSklad() {
+        assertEquals("OK", uv.processInput("use sk"));
+        tests();
+        testSklad(true);
+    }
+
+    @Test
+    public void testUsePrioritetnaVrsta() {
+        assertEquals("OK", uv.processInput("use pv"));
+        tests();
+        testPrioritetnaVrsta(true);
+    }
+
+    @Test
+    public void testUseBst() {
+        assertEquals("OK", uv.processInput("use bst"));
+        tests();
+        testBst(true);
+    }
+
+    // *****************
+    // POMOZNE METODE
+    // *****************
+
+    private void tests() {
+
+        testAddNothing();
+        testRemoveFirstNothing();
+        testGetFirstNothing();
+        testDepthOnEmpty();
+        testIsEmptyEmpty();
+        testResetOnEmpty();
+        testSizeOnEmpty();
+
+        reset();
+        testSizeOne();
+
+        reset();
+        testDepthOne();
+
+        reset();
+        testGetFirst();
+
+        reset();
+        testRemoveFirst();
+
+        reset();
+        testSizeTwo();
+
+        reset();
+        testDepthTwo();
+
+        reset();
+        testIsEmptyNotEmpty();
+
+        reset();
+        testResetOnFull();
+
+        reset();
+        testExists();
+
+        reset();
+        testExistsTwo();
+
+        reset();
+        testExistsOnEmpty();
+
+        reset();
+        testExistsNonExistentOne();
+
+        reset();
+        testExistsNonExistent();
+
+        reset();
+        testRemove();
+
+        reset();
+        testRemoveTwo();
+
+        reset();
+        testRemoveEmpty();
+
+        reset();
+        testRemoveNonExistent();
+
+        reset();
+        testRemoveTwoNonExistent();
+
+
+        reset();
+    }
+
+    public void reset() {
+        uv.processInput("reset");
+    }
+
+    public void testAdd() {
+        assertEquals("OK", uv.processInput("add Test1"));
+        assertEquals("OK", uv.processInput("add Test2"));
+    }
+
+    public void testAddNothing() {
+        assertEquals("Error: please specify a string", uv.processInput("add"));
+    }
+
+    public void testRemoveFirst() {
+        assertEquals("OK", uv.processInput("add Test"));
+        assertEquals("Test", uv.processInput("remove_first"));
+    }
+
+    public void testRemoveFirstNothing() {
+        assertEquals("Error: data structure is empty", uv.processInput("remove_first"));
+        assertEquals("Error: please specify a string", uv.processInput("add"));
+        assertEquals("Error: data structure is empty", uv.processInput("remove_first"));
+    }
+
+    public void testGetFirst() {
+        assertEquals("OK", uv.processInput("add Test"));
+        assertEquals("Test", uv.processInput("get_first"));
+    }
+
+    public void testGetFirstNothing() {
+        assertEquals("Error: data structure is empty", uv.processInput("get_first"));
+        assertEquals("Error: please specify a string", uv.processInput("add"));
+        assertEquals("Error: data structure is empty", uv.processInput("get_first"));
+    }
+
+    public void testSizeOnEmpty() {
+        assertEquals("0", uv.processInput("size"));
+    }
+
+    public void testSizeOne() {
+        assertEquals("OK", uv.processInput("add Test"));
+        assertEquals("1", uv.processInput("size"));
+    }
+
+    public void testSizeTwo() {
+        assertEquals("OK", uv.processInput("add Test1"));
+        assertEquals("OK", uv.processInput("add Test2"));
+        assertEquals("2", uv.processInput("size"));
+    }
+
+    public void testDepthOnEmpty() {
+        assertEquals("0", uv.processInput("depth"));
+    }
+
+    public void testDepthOne() {
+        assertEquals("OK", uv.processInput("add Test"));
+        assertEquals("1", uv.processInput("depth"));
+    }
+
+    public void testDepthTwo() {
+        assertEquals("OK", uv.processInput("add Test1"));
+        assertEquals("OK", uv.processInput("add Test2"));
+        assertEquals("2", uv.processInput("depth"));
+    }
+
+    public void testIsEmptyEmpty() {
+        assertEquals("Data structure is empty.", uv.processInput("is_empty"));
+        assertEquals("Error: please specify a string", uv.processInput("add"));
+        assertEquals("Data structure is empty.", uv.processInput("is_empty"));
+    }
+
+    public void testIsEmptyNotEmpty() {
+        assertEquals("OK", uv.processInput("add Test1"));
+        assertEquals("OK", uv.processInput("add Test2"));
+        assertEquals("OK", uv.processInput("add Test3"));
+        assertEquals("Data structure is not empty.", uv.processInput("is_empty"));
+    }
+
+    public void testResetOnEmpty() {
+        assertEquals("OK", uv.processInput("reset"));
+    }
+
+    public void testResetOnFull() {
+        assertEquals("OK", uv.processInput("add Test"));
+        assertEquals("OK", uv.processInput("reset"));
+        assertEquals("Error: data structure is empty", uv.processInput("remove_first"));
+        assertEquals("0", uv.processInput("size"));
+    }
+
+    // TO DO
+    public void testRemove() {
+        assertEquals("OK", uv.processInput("add Test1"));
+        assertEquals("Test1", uv.processInput("remove Test1"));
+        assertEquals("0", uv.processInput("size"));
+    }
+
+    public void testRemoveTwo() {
+        testAdd();
+        assertEquals("Test2", uv.processInput("remove Test2"));
+        assertEquals("Test1", uv.processInput("remove Test1"));
+        assertEquals("0", uv.processInput("size"));
+    }
+
+    public void testRemoveEmpty() {
+        assertEquals("Error: can't remove element that doesn't exist in data structure.", uv.processInput("remove Test2"));
+    }
+
+    public void testRemoveNonExistent() {
+        assertEquals("OK", uv.processInput("add Test1"));
+        assertEquals("Error: can't remove element that doesn't exist in data structure.", uv.processInput("remove Test2"));
+        assertEquals("1", uv.processInput("size"));
+    }
+
+    public void testRemoveTwoNonExistent() {
+        testAdd();
+        assertEquals("Error: can't remove element that doesn't exist in data structure.", uv.processInput("remove Test3"));
+    }
+
+    public void testExists() {
+        assertEquals("OK", uv.processInput("add Test1"));
+        assertEquals("Element exists in data structure.", uv.processInput("exists Test1"));
+    }
+
+    public void testExistsOnEmpty() {
+        assertEquals("Element doesn't exist in data structure.", uv.processInput("exists Test"));
+    }
+
+
+    public void testExistsTwo() {
+        testAdd();
+        assertEquals("Element exists in data structure.", uv.processInput("exists Test1"));
+        assertEquals("Element exists in data structure.", uv.processInput("exists Test2"));
+    }
+
+
+    public void testExistsNonExistentOne() {
+        assertEquals("OK", uv.processInput("add Test1"));
+        assertEquals("Element doesn't exist in data structure.", uv.processInput("exists Test2"));
+    }
+
+
+    public void testExistsNonExistent() {
+        testAdd();
+        assertEquals("Element doesn't exist in data structure.", uv.processInput("exists Test3"));
+    }
+
+
+    public void testAddTestSequence() {
+        assertEquals("OK", uv.processInput("add Test4"));
+        assertEquals("OK", uv.processInput("add Test2"));
+        assertEquals("OK", uv.processInput("add Test3"));
+        assertEquals("OK", uv.processInput("add Test1"));
+        assertEquals("OK", uv.processInput("add Test5"));
+    }
+
+    public void testSklad(boolean add) {
+        if (add) {
+            testAddTestSequence();
+        }
+        assertEquals("Test5", uv.processInput("remove_first"));
+        assertEquals("Test1", uv.processInput("remove_first"));
+        assertEquals("Test3", uv.processInput("remove_first"));
+        assertEquals("Test2", uv.processInput("remove_first"));
+        assertEquals("Test4", uv.processInput("remove_first"));
+    }
+
+    public void testPrioritetnaVrsta(boolean add) {
+        if (add) {
+            testAddTestSequence();
+        }
+        assertEquals("Test5", uv.processInput("remove_first"));
+        assertEquals("Test4", uv.processInput("remove_first"));
+        assertEquals("Test3", uv.processInput("remove_first"));
+        assertEquals("Test2", uv.processInput("remove_first"));
+        assertEquals("Test1", uv.processInput("remove_first"));
+    }
+
+    public void testBst(boolean add) {
+        if (add) {
+            testAddTestSequence();
+        }
+        assertEquals("Test4", uv.processInput("remove_first"));
+        assertEquals("Test5", uv.processInput("remove_first"));
+        assertEquals("Test2", uv.processInput("remove_first"));
+        assertEquals("Test3", uv.processInput("remove_first"));
+        assertEquals("Test1", uv.processInput("remove_first"));
+    }
+
+}
