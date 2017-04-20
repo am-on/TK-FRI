a354409baabd4b531eae7a25f281067464d582e5
am-on <igre@outlook.com>
2017-04-20 12:00:41 +0200

    dn-1 code init

---
+++ dn-1/src/SeznamiUV.java
@@ -0,0 +1,86 @@
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class SeznamiUV {
+
+    HashMap<String, Seznam<String>> seznami;
+    Seznam<String> seznam;
+
+    public SeznamiUV() {
+        seznami = new HashMap<>();
+        seznami.put("pv", new PrioritetnaVrsta<String>());
+        seznami.put("sk", new Sklad<String>());
+        seznami.put("bst", new Bst<String>());
+    }
+
+    public String processInput(String input) {
+        Scanner sc = new Scanner(input);
+        String token;
+        String result = "OK";
+
+        if (sc.hasNext()) {
+            token = sc.next();
+        } else {
+            return "Error: enter command";
+        }
+        if (!token.equals("use") && (null == seznam)) {
+            return "Error: please specify a data structure (use {pv|sk|bst})";
+        }
+        try {
+            if (token.equals("use")) {
+                if (sc.hasNext()) {
+                    seznam = seznami.get(sc.next());
+                    if (null == seznam) {
+                        result = "Error: please specify a correct data structure type {pv|sk|bst}";
+                    }
+                } else {
+                    result = "Error: please specify a data structure type {pv|sk|bst}";
+                }
+            } else if (token.equals("add")) {
+                if (sc.hasNext()) {
+                    seznam.add(sc.next());
+                } else {
+                    result = "Error: please specify a string";
+                }
+            } else if (token.equals("remove_first")) {
+                result = seznam.removeFirst();
+            } else if (token.equals("get_first")) {
+                result = seznam.getFirst();
+            } else if (token.equals("size")) {
+                result = seznam.size() + "";
+            } else if (token.equals("depth")) {
+                result = seznam.depth() + "";
+            } else if (token.equals("is_empty")) {
+                result = "Data structure is " + (seznam.isEmpty() ? "" : "not ") + "empty.";
+            } else if (token.equals("reset")) {
+                while (!seznam.isEmpty()) {
+                    seznam.removeFirst();
+                }
+            } else if (token.equals("exists")) {
+                if (sc.hasNext()) {
+                    result = "Element " + (seznam.exists(sc.next()) ? "exists " : "doesn't exist ") + "in data structure.";
+                } else {
+                    result = "Error: please specify a string";
+                }
+            } else if (token.equals("remove")) {
+                if (sc.hasNext()) {
+                    String next = sc.next();
+                    if (!seznam.exists(next))
+                        result = "Error: can't remove element that doesn't exist in data structure.";
+                    else
+                        result = seznam.remove(next);
+                } else {
+                    result = "Error: please specify a string";
+                }
+            } else {
+                result = "Error: invalid command";
+            }
+        }  catch (IllegalArgumentException e) {
+            result = "Error: Duplicated entry";
+        } catch (java.util.NoSuchElementException e) {
+            result = "Error: data structure is empty";
+        }
+        return result;
+    }
+}
