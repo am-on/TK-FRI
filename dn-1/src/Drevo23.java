a2185cf57979fd5377f6fc545162088288aa3743
am-on <igre@outlook.com>
2017-05-07 19:08:17 +0200

    implement add function

--- dn-1/src/Drevo23.java
+++ dn-1/src/Drevo23.java
@@ -1,4 +1,4 @@
-
+import java.util.Vector;
 
 /**
  * Created by amon on 22.4.2017.
@@ -16,8 +16,7 @@ public class Drevo23 <Tip extends Comparable> implements Seznam<Tip> {
     private class InnerNode extends Node {
         public Tip valueLeft;
         public Tip valueRight;
-
-        public Node parent;
+        
         public Node left;
         public Node middle;
         public Node right;
@@ -104,7 +103,6 @@ public class Drevo23 <Tip extends Comparable> implements Seznam<Tip> {
 
         if (size == 0) {
             root = new LeafNode(null, e);
-
         } else if (size == 1) {
             LeafNode l1 = (LeafNode) root;
             LeafNode l2 = new LeafNode(null, e);
@@ -125,7 +123,7 @@ public class Drevo23 <Tip extends Comparable> implements Seznam<Tip> {
             }
 
         } else {
-            LeafNode ln = new LeafNode(null, e);
+            LeafNode newNode = new LeafNode(null, e);
 
             Node n = getRoot();
             while (true) {
@@ -133,7 +131,8 @@ public class Drevo23 <Tip extends Comparable> implements Seznam<Tip> {
                 if (in.left.getClass() == LeafNode.class) {
                     break;
                 }
-                int cmp = in.compareTo(ln);
+
+                int cmp = in.compareTo(newNode);
                 if (cmp < 0) {
                     n = in.left;
                 } else if (cmp == 0) {
@@ -147,22 +146,22 @@ public class Drevo23 <Tip extends Comparable> implements Seznam<Tip> {
 
             // right leaf is empty, we can insert in this node
             if (in.canInsert()) {
-                ln.parent = in;
+                newNode.parent = in;
 
                 // new element is larger than middle, insert it to right
-                if (ln.compareTo(in.middle) > 0) {
+                if (newNode.compareTo(in.middle) > 0) {
                     System.out.println("HERE" + in.valueLeft);
-                    in.right = ln;
-                    in.valueRight = ln.value;
+                    in.right = newNode;
+                    in.valueRight = newNode.value;
                 } else {
                     // middle element is the largest, move it to right
                     in.right = in.middle;
                     in.valueRight = ((LeafNode)in.middle).value;
 
                     // new element is larger than left, insert it to middle
-                    if(ln.compareTo(in.left) > 0) {
-                        in.middle = ln;
-                        in.valueLeft = ln.value;
+                    if(newNode.compareTo(in.left) > 0) {
+                        in.middle = newNode;
+                        in.valueLeft = newNode.value;
                     }
                     // new element is the smallest of elements in node
                     else {
@@ -171,26 +170,163 @@ public class Drevo23 <Tip extends Comparable> implements Seznam<Tip> {
                         in.valueLeft = ((LeafNode)in.left).value;
 
                         // set new element to left
-                        in.left = ln;
+                        in.left = newNode;
 
+                        // update upper nodes
+                        upperUpdate(in);
                     }
+                }
+            }
+            // inner node is full, split it into two nodes
+            else {
+                Vector<LeafNode> lv = new Vector<>(4);
 
+                // add already inserted nodes to vector
+                lv.add((LeafNode) in.left);
+                lv.add((LeafNode) in.middle);
+                lv.add((LeafNode) in.right);
+
+                // add new node to vector
+                for (int i = 2; i >= 0; i--) {
+                    // if node is larger than current element, insert it on index after current element
+                    if (newNode.compareTo(lv.get(i)) > 0) {
+                        lv.insertElementAt(newNode, i+1);
+                        break;
+                    }
+                }
+                // if new node wasn't inserted insert it to index 0
+                if (lv.size() < 4) {
+                    lv.insertElementAt(newNode, 0);
                 }
 
-                //TODO ce vstavljas v in.left, bo mogoce treba visje posodobiti in.leftValue
+                // create two new inner nodes
+                InnerNode leftN = createInnerNode(lv.elementAt(0), lv.elementAt(1));
+                InnerNode rightN = createInnerNode(lv.elementAt(2), lv.elementAt(3));
+                InnerNode parent = (InnerNode) in.parent;
 
+                leftN.parent = parent;
+                rightN.parent = parent;
 
-            } else {
-                throw new UnsupportedOperationException("TODO");
+                if (parent == null) {
+                    root = createInnerNode(leftN, rightN);
+                } else if (parent.canInsert()) {
+                    insertToParent(parent, in, leftN, rightN);
+                } else {
+                    // not enough space, do a split <- this could repeat to top of the tree!
+
+                    while (true) {
+                        InnerNode left;
+                        InnerNode right;
+
+                        if (parent.left == in) {
+                            left = createInnerNode(leftN, rightN);
+                            right = createInnerNode(parent.middle, parent.right);
+                        } else if (parent.middle == in) {
+                            left = createInnerNode(parent.left, leftN);
+                            right = createInnerNode(rightN, parent.right);
+                        } else {
+                            left = createInnerNode(parent.left, parent.middle);
+                            right = createInnerNode(leftN, rightN);
+                        }
+
+                        // there is nothing above this nodes, create new node and make it root
+                        if (parent.parent == null) {
+                            root = createInnerNode(left, right);
+                            left.parent = root;
+                            right.parent = root;
+                            break;
+                        }
+                        // parent exists, but could be full
+                        else {
+                            leftN = left;
+                            rightN = right;
+                            in = parent;
+                            parent = (InnerNode) parent.parent;
+
+                            // there is enough space to insert in parent
+                            if (parent.canInsert()) {
+                                insertToParent(parent, in, leftN, rightN);
+                                break;
+                            }
+
+                        }
+                    }
+                }
             }
+        }
+        size++;
+    }
 
+    private void insertToParent(InnerNode parent, InnerNode beforeSplit, InnerNode left, InnerNode right) {
+        // check if split node was left or middle, insert new nodes to place where they belong
+        if (parent.left == beforeSplit) {
+            // move middle to right
+            parent.right = parent.middle;
+            parent.valueRight = parent.valueLeft;
 
+            // insert split nodes
+            parent.left = left;
+            parent.middle = right;
 
+            // update left value with middle minimum
+            parent.valueLeft = getMin(parent.middle);
+        } else {
+            // insert split nodes
+            parent.middle = left;
+            parent.right = right;
 
+            // update values in parent node
+            parent.valueLeft = getMin(parent.middle);
+            parent.valueRight = getMin(parent.right);
         }
 
-        size++;
+        left.parent = parent;
+        right.parent = parent;
+    }
 
+    private Tip getMin(Node n) {
+        while (n.getClass() == InnerNode.class) {
+            n = ((InnerNode)n).left;
+        }
+        return ((LeafNode)n).value;
+    }
+
+    private InnerNode createInnerNode(Node left, Node middle) {
+        InnerNode in = new InnerNode(null);
+
+        in.valueLeft = getMin(middle);
+        in.left = left;
+        in.middle = middle;
+
+        in.left.parent = in;
+        in.middle.parent = in;
+
+        return in;
+    }
+
+    private void upperUpdate(InnerNode in) {
+        Tip value = ((LeafNode)in.left).value;
+
+        InnerNode before = in;
+
+        while (in.parent != null) {
+            InnerNode parent = (InnerNode) in.parent;
+
+            // if node is parents left node, go upper
+            if (parent.left == in) {
+                in = parent;
+            }
+            // if node is parents middle node, update parents left value
+            else if (parent.middle == in) {
+                parent.valueLeft = value;
+                break;
+            }
+            // if node is parent right node, update parents right value
+            else if (parent.right == in) {
+                parent.valueRight = value;
+                break;
+            }
+        }
     }
 
     @Override
@@ -203,14 +339,7 @@ public class Drevo23 <Tip extends Comparable> implements Seznam<Tip> {
         if (size == 0) {
             throw new java.util.NoSuchElementException();
         }
-
-        Node n = root;
-
-        while (n.getClass() == InnerNode.class) {
-            n = ((InnerNode)n).left;
-        }
-
-        return ((LeafNode)n).value;
+        return getMin(root);
     }
 
     @Override
