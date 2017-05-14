b8368579e59ff40714e47f45ac53218b5e8cff58
am-on <igre@outlook.com>
2017-05-14 21:07:18 +0200

    improve tests

--- dn-1/test/Drevo23Test.java
+++ dn-1/test/Drevo23Test.java
@@ -156,167 +156,20 @@ public class Drevo23Test {
 
     @Test
     public void testAddManyMiddle() {
+        instance.add("a");
         instance.add("l");
+        instance.add("b");
         instance.add("k");
         instance.add("j");
-        instance.add("i");
-        instance.add("h");
-        instance.add("g");
-        instance.add("f");
-        instance.add("e");
-        instance.add("d");
-        instance.add("c");
-        instance.add("b");
-        instance.add("a");
-        instance.add("f0");
-        instance.add("g0");
-        instance.add("g1");
-        instance.add("g2");
-        instance.add("g3");
-        instance.add("f3");
-        instance.add("f4");
-        assertEquals("[\"a\", \"b\", \"c\", \"d\", \"e\", \"f\", \"f0\", \"f3\", \"f4\", \"g\", \"g0\", \"g1\", \"g2\", \"g3\", \"h\", \"i\", \"j\", \"k\", \"l\"]", instance.asList());
-    }
-
-    @Test
-    public void testAddM() {
-        instance.add("d0xa");
-        instance.add("e0xa");
-        instance.add("m0xa");
-        instance.add("n0xa");
-        instance.add("a");
-        instance.add("b");
         instance.add("c");
         instance.add("d");
+        instance.add("i");
         instance.add("e");
-        instance.add("m");
-        instance.add("a0xa");
-        instance.add("b0xa");
-        instance.add("c0xa");
-        instance.add("o0xa");
-        instance.add("p0xa");
-        instance.add("r0xa");
-        instance.add("s0xa");
-        instance.add("t0xa");
-        instance.add("u0xa");
-        instance.add("f0xa");
-        instance.add("g0xa");
-        instance.add("h0xa");
-        instance.add("i0xa");
-        instance.add("j0xa");
-        instance.add("k0xa");
-        instance.add("l0xa");
-        instance.add("00xa");
-        instance.add("70xa");
-        instance.add("80xa");
-        instance.add("90xa");
-        instance.add("n");
-        instance.add("o");
-        instance.add("p");
-        instance.add("r");
-        instance.add("s");
-        instance.add("10xa");
-        instance.add("20xa");
-        instance.add("30xa");
-        instance.add("40xa");
-        instance.add("50xa");
-        instance.add("60xa");
-        instance.add("t");
-        instance.add("u");
-        instance.add("a0");
-        instance.add("b0");
-        instance.add("c0");
-        instance.add("d0");
-        instance.add("e0");
-        instance.add("m0");
-        instance.add("n0");
-        instance.add("o0");
-        instance.add("p0");
-        instance.add("a0x");
-        instance.add("b0x");
-        instance.add("c0x");
-        instance.add("d0x");
-        instance.add("e0x");
-        instance.add("m0x");
-        instance.add("n0x");
-        instance.add("o0x");
-        instance.add("p0x");
-        instance.add("r0x");
-        instance.add("s0x");
-        instance.add("t0x");
-        instance.add("u0x");
-        instance.add("f0x");
-        instance.add("g0x");
-        instance.add("h0x");
-        instance.add("i0x");
-        instance.add("j0x");
-        instance.add("k0x");
-        instance.add("l0x");
-        instance.add("00x");
-        instance.add("10x");
-        instance.add("1");
-        instance.add("2");
-        instance.add("3");
-        instance.add("4");
-        instance.add("5");
-        instance.add("20x");
-        instance.add("30x");
-        instance.add("40x");
-        instance.add("50x");
-        instance.add("60x");
-        instance.add("70x");
-        instance.add("80x");
-        instance.add("90x");
-        instance.add("r0");
-        instance.add("s0");
-        instance.add("t0");
-        instance.add("u0");
-        instance.add("f0");
-        instance.add("g0");
-        instance.add("h0");
-        instance.add("i0");
-        instance.add("j0");
-        instance.add("k0");
-        instance.add("l0");
-        instance.add("00");
-        instance.add("10");
-        instance.add("20");
-        instance.add("30");
-        instance.add("40");
-        instance.add("50");
-        instance.add("60");
-        instance.add("70");
-        instance.add("80");
-        instance.add("90");
+        instance.add("h");
         instance.add("f");
         instance.add("g");
-        instance.add("h");
-        instance.add("i");
-        instance.add("j");
-        instance.add("k");
-        instance.add("l");
-        instance.add("0");
-        instance.add("6");
-        instance.add("7");
-        instance.add("8");
-        instance.add("9");
-
-        assertEquals("[\"0\", \"00\", \"00x\", \"00xa\", \"1\", \"10\", \"10x\", \"10xa\", \"2\", \"20\", " +
-                        "\"20x\", \"20xa\", \"3\", \"30\", \"30x\", \"30xa\", \"4\", \"40\", \"40x\", \"40xa\", " +
-                        "\"5\", \"50\", \"50x\", \"50xa\", \"6\", \"60\", \"60x\", \"60xa\", \"7\", \"70\", \"70x\", " +
-                        "\"70xa\", \"8\", \"80\", \"80x\", \"80xa\", \"9\", \"90\", \"90x\", \"90xa\", \"a\", " +
-                        "\"a0\", \"a0x\", \"a0xa\", \"b\", \"b0\", \"b0x\", \"b0xa\", \"c\", \"c0\", \"c0x\", " +
-                        "\"c0xa\", \"d\", \"d0\", \"d0x\", \"d0xa\", \"e\", \"e0\", \"e0x\", \"e0xa\", \"f\", " +
-                        "\"f0\", \"f0x\", \"f0xa\", \"g\", \"g0\", \"g0x\", \"g0xa\", \"h\", \"h0\", \"h0x\", " +
-                        "\"h0xa\", \"i\", \"i0\", \"i0x\", \"i0xa\", \"j\", \"j0\", \"j0x\", \"j0xa\", \"k\", " +
-                        "\"k0\", \"k0x\", \"k0xa\", \"l\", \"l0\", \"l0x\", \"l0xa\", \"m\", \"m0\", \"m0x\", " +
-                        "\"m0xa\", \"n\", \"n0\", \"n0x\", \"n0xa\", \"o\", \"o0\", \"o0x\", \"o0xa\", \"p\", " +
-                        "\"p0\", \"p0x\", \"p0xa\", \"r\", \"r0\", \"r0x\", \"r0xa\", \"s\", \"s0\", \"s0x\", " +
-                        "\"s0xa\", \"t\", \"t0\", \"t0x\", \"t0xa\", \"u\", \"u0\", \"u0x\", \"u0xa\"]",
-                    instance.asList());
-
+        assertEquals("[\"a\", \"b\", \"c\", \"d\", \"e\", \"f\", \"g\", \"h\", \"i\", \"j\", \"k\", \"l\"]", instance.asList());
     }
-
 
     // EXISTS test
 
@@ -569,6 +422,57 @@ public class Drevo23Test {
         instance.remove("Test");
     }
 
+    @Test
+    public void testRemoveRight () {
+        instance.add("a");
+        instance.add("b");
+        instance.add("c");
+        instance.add("d");
+        instance.add("e");
+        instance.add("f");
+        instance.add("g");
+        instance.add("h");
+
+        assertEquals("h", instance.remove("h"));
+        assertEquals("[\"a\", \"b\", \"c\", \"d\", \"e\", \"f\", \"g\"]", instance.asList());
+        assertEquals("g", instance.remove("g"));
+        assertEquals("[\"a\", \"b\", \"c\", \"d\", \"e\", \"f\"]", instance.asList());
+        assertEquals("f", instance.remove("f"));
+        assertEquals("[\"a\", \"b\", \"c\", \"d\", \"e\"]", instance.asList());
+        assertEquals("e", instance.remove("e"));
+        assertEquals("[\"a\", \"b\", \"c\", \"d\"]", instance.asList());
+        assertEquals("d", instance.remove("d"));
+        assertEquals("[\"a\", \"b\", \"c\"]", instance.asList());
+        assertEquals("c", instance.remove("c"));
+        assertEquals("[\"a\", \"b\"]", instance.asList());
+        assertEquals("b", instance.remove("b"));
+        assertEquals("[\"a\", ]", instance.asList());
+        assertEquals("a", instance.remove("a"));
+    }
+
+    @Test
+    public void testRemoveMiddle () {
+        instance.add("a");
+        instance.add("b");
+        instance.add("c");
+        instance.add("d");
+        instance.add("e");
+        instance.add("f");
+        instance.add("g");
+        instance.add("h");
+
+        assertEquals("d", instance.remove("d"));
+        assertEquals("[\"a\", \"b\", \"c\", \"e\", \"f\", \"g\", \"h\"]", instance.asList());
+        assertEquals("c", instance.remove("c"));
+        assertEquals("[\"a\", \"b\", \"e\", \"f\", \"g\", \"h\"]", instance.asList());
+        assertEquals("e", instance.remove("e"));
+        assertEquals("[\"a\", \"b\", \"f\", \"g\", \"h\"]", instance.asList());
+        assertEquals("b", instance.remove("b"));
+        assertEquals("[\"a\", \"f\", \"g\", \"h\"]", instance.asList());
+        assertEquals("f", instance.remove("f"));
+        assertEquals("[\"a\", \"g\", \"h\"]", instance.asList());
+    }
+
 
     // REMOVE_FIRST tests
 
@@ -601,11 +505,17 @@ public class Drevo23Test {
     public void testRemoveFirstMany() {
         addMany();
 
+        System.out.println(instance.asList());
+
         assertEquals("Test0", instance.removeFirst());
         assertFalse(instance.exists("Test0"));
 
+        System.out.println(instance.asList());
+
         assertEquals("Test1", instance.removeFirst());
         assertFalse(instance.exists("Test1"));
+
+        System.out.println(instance.asList());
 
         assertEquals("Test3", instance.removeFirst());
         assertFalse(instance.exists("Test3"));
